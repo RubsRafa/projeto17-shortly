ac738837ebd78a92a599bae6a5f249487f908a3f
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-24 12:09:27 -0300

    feat: add database

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Servidor funcionando na porta ${port}`))
