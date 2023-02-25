c932df46e289fae3f866a5b322fd438772e696b3
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-25 19:23:38 -0300

    feat: start of post url

---
+++ src/middlewares/postUrlShortValidation.js
@@ -0,0 +1,23 @@
+import { db } from '../database/db.js';
+
+export async function postUrlShortValidation(req, res, next) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    const { url } = req.body;
+
+    try {
+        if (!authorization) return res.sendStatus(401);
+
+        if (!token || token === "" || typeof(token) !== "string") return res.sendStatus(401);
+        if (!url || url === "" || typeof(url) !== "string") return res.sendStatus(422);
+
+        const tokenExist = await db.query('SELECT * FROM sessions WHERE token = $1;', [token]);
+        if (!tokenExist.rows[0]) return res.sendStatus(422);
+
+        next(); 
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+}
