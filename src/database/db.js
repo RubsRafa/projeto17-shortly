b026276b49f822309ff6d4bbb1c113693a391f30
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-27 16:02:41 -0300

    test: ssl false

--- src/database/db.js
+++ src/database/db.js
@@ -4,18 +4,15 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const configDatabase = {
-//     connectionString: process.env.DATABASE_URL,
-//     ...(process.env.NODE_ENV === "prod" && {
-//         ssl: {
-//             rejectUnauthorized: false,
-//         },
-//     }),
-// };
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL,
+    ...(process.env.NODE_ENV === "prod" && {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    }),
+};
 
 // if (process.env.NODE_ENV === 'prod') configDatabase.ssl = true;
 
-export const db = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
+export const db = new Pool(configDatabase);
