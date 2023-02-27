366037f46473220543b108269ab9adad8cfd6a7b
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-27 15:45:03 -0300

    refactor: connection string and ssl

--- src/database/db.js
+++ src/database/db.js
@@ -4,15 +4,18 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const configDatabase = {
-    connectionString: process.env.DATABASE_URL,
-    ...(process.env.NODE_ENV === "prod" && {
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    }),
-};
+// const configDatabase = {
+//     connectionString: process.env.DATABASE_URL,
+//     ...(process.env.NODE_ENV === "prod" && {
+//         ssl: {
+//             rejectUnauthorized: false,
+//         },
+//     }),
+// };
 
 // if (process.env.NODE_ENV === 'prod') configDatabase.ssl = true;
 
-export const db = new Pool(configDatabase);
+export const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
